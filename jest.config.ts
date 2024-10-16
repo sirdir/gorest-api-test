2aaa67e934b9082e6d282fa201d9dff8ec034b68
sirdir <phddir@gmail.com>
2024-10-16 18:58:24 +0200

    add prettier

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,5 @@
 export default {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  setupFiles: ["dotenv/config"]
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  setupFiles: ['dotenv/config'],
 };
