8ecfc313b7e28ac480a0fce0ee41681fa4ecd06b
sirdir <phddir@gmail.com>
2024-10-17 08:53:04 +0200

    extend coverage for /user endpoint

--- src/tests/userCRUD.test.ts
+++ src/tests/userCRUD.test.ts
@@ -1,25 +1,47 @@
 import { apiClient } from '../utils/apiClient';
 
 describe('GoRest User CRUD Operations', () => {
-  let userId: number;
+  const userData = {
+    name: 'John Doe',
+    email: `john.doe${Math.random() * 1_000_000}@mail.com`,
+    gender: 'male',
+    status: 'active',
+  };
+  let id: number;
 
   it('should create a new user', async () => {
-    const userData = {
-      name: 'John Doe',
-      email: `john.doe${Math.random() * 1_000_000}@mail.com`,
-      gender: 'male',
-      status: 'active',
-    };
-
-    const response = await apiClient.post('/users', userData);
-    expect(response.status).toBe(201);
-    expect(response.body).toHaveProperty('id');
-    userId = response.body.id;
+    await apiClient
+      .post('/users', userData)
+      .expect(201)
+      .then((response) => {
+        expect(response.body).toHaveProperty('id');
+        expect(response.body).toMatchObject(userData);
+        id = response.body.id;
+      });
   });
 
-  it('should retrieve user details', async () => {});
+  it('should retrieve user details', async () => {
+    await apiClient
+      .get(`/users/${id}`)
+      .expect(200)
+      .then((response) => {
+        expect(response.body).toStrictEqual({ ...userData, id });
+      });
+  });
 
-  it('should update the user details', async () => {});
+  it('should update the user details', async () => {
+    const updatedData = {
+      name: 'John Doe Updated',
+      status: 'inactive',
+    };
 
-  it('should delete the user', async () => {});
+    await apiClient
+      .put(`/users/${id}`, updatedData)
+      .expect(200)
+      .then((response) => expect(response.body).toStrictEqual({ ...userData, ...updatedData, id }));
+  });
+
+  it('should delete the user', async () => {
+    await apiClient.delete(`/users/${id}`).expect(204);
+  });
 });
